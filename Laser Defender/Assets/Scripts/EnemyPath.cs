a0d21a779f0960a322ab3fa64470699c597a945d
Rinita Nair <rnair3@uncc.edu>
2019-07-12 23:13:52 -0400

    Creating enemy waypoint

---
+++ Laser Defender/Assets/Scripts/EnemyPath.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPath : MonoBehaviour
+{
+
+    [SerializeField] List<Transform> waypoints;
+    [SerializeField] float moveSpeed = 2f;
+
+    int waypointIndex = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = waypoints[waypointIndex].transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveEnemyUsingWaypoints();
+
+    }
+
+    private void MoveEnemyUsingWaypoints()
+    {
+        if (waypointIndex <= waypoints.Count - 1)
+        {
+            var targetPos = waypoints[waypointIndex].transform.position;
+            var movemnetThisFrame = moveSpeed * Time.deltaTime;
+
+            transform.position = Vector2.MoveTowards(transform.position, targetPos, movemnetThisFrame);
+
+            if (transform.position == targetPos)
+            {
+                waypointIndex++;
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
